73ef24efafbba4541e73555044a3dc3324e501ed
Dale Karp <dale@dale.io>
2015-08-28 02:08:58 -0400

    updated run script to make testing less annoying

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,11 +1,13 @@
-# start kafka if not running
-kafka_running=$(sudo su kafka -c jps | grep Kafka)
-if [ $? -eq 1 ]
+unamestr=`uname`
+if [[ "$unamestr" != "Darwin" ]]
 then
-    sudo su kafka <<EOF
+    # start kafka if not running
+    kafka_running=$(sudo su kafka -c jps | grep Kafka)
+    if [ $? -eq 1 ]
+    then
+        sudo su kafka <<EOF
 nohup ~/kafka/bin/kafka-server-start.sh ~/kafka/config/server.properties > ~/kafka/kafka.log 2>&1 &
 echo "TOPIC_TOUCH" | ~/kafka/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic TutorialTopic > /dev/null
 EOF
+    fi
 fi
-
-# postgres is already running automatically, care of ubuntu
